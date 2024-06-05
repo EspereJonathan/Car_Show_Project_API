bd12b18393ec4e4e27b23dc39833ca488c19cd98
EspereJonathan <hei.espere@gmail.com>
2024-06-05 05:35:13 +0300

    feat: init project

---
+++ src/main/java/com/espere/car_show/CarShowApplication.java
@@ -0,0 +1,13 @@
+package com.espere.car_show;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarShowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/espere/car_show/CarShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espere.car_show;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
